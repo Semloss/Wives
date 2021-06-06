1e0f169e7b2896cd3484c3ca47fc52836eed0d58
semonchang <445671079@qq.com>
2021-06-06 19:14:37 +0800

    Initial Commit

---
+++ Wives/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wives
+//
+//  Created by 张松 on 2021/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wives/WivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WivesApp.swift
+//  Wives
+//
+//  Created by 张松 on 2021/6/6.
+//
+
+import SwiftUI
+
+@main
+struct WivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
